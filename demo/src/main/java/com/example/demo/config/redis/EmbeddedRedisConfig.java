40ee342c2760e047cdc6a0a490d6e734cd8b4583
0yeap95d <tjdwob@gmail.com>
2021-01-18 01:37:19 +0900

    update redis config

--- demo/src/main/java/com/example/demo/config/redis/EmbeddedRedisConfig.java
+++ demo/src/main/java/com/example/demo/config/redis/EmbeddedRedisConfig.java
@@ -1,5 +1,33 @@
 package com.example.demo.config.redis;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import lombok.RequiredArgsConstructor;
+import redis.embedded.RedisServer;
+
+@RequiredArgsConstructor
+@Profile("local")
+@Configuration
 public class EmbeddedRedisConfig {
 
+    private final RedisProperties redisProperties;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = new RedisServer(redisProperties.getPort());
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
 }
