98f7df7e66bbac05b2b287dd70b567e4f164503d
Jaehyeok <hyeok9sae@gmail.com>
2021-01-19 12:20:08 +0900

    create | test-board

--- demo/src/main/java/com/example/demo/service/board/BoardService.java
+++ demo/src/main/java/com/example/demo/service/board/BoardService.java
@@ -1,5 +1,10 @@
 package com.example.demo.service.board;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boards")
 public class BoardService {
 
 }
