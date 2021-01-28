107b843923c508a9bb97c212bb72038197b8f0d1
yeoro <dufgh1009@naver.com>
2021-01-28 17:03:45 +0900

    Implement | equipment entity

--- demo/src/main/java/com/example/demo/entity/equipment/Equipment.java
+++ demo/src/main/java/com/example/demo/entity/equipment/Equipment.java
@@ -1,5 +1,14 @@
 package com.example.demo.entity.equipment;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Equipment {
 	
 	// 장비 이름
